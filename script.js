859d7e2eb8a3adc5e70107d505ac166ad2261aec
shrikantmamale <63542170+shrikantmamale@users.noreply.github.com>
2021-05-07 13:25:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-  
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
+
+            function compute()
+            {
+                p = document.getElementById("p").value;
+                n = document.getElementById("n").value;
+                r = document.getElementById("r").value;
+                result = document.getElementById("result");
+                
+                result.innerHTML = "The interest is " + (p*n*r/100);
+            }
+        
